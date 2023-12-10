457ac6b52aff2ca0cd97e73d57f4333521d25cee
Ana <ana.mgholandadev@gmail.com>
2023-12-09 22:20:53 -0300

    Verificação estática de CharactersSame

--- src/main/java/com/thealgorithms/strings/CharactersSame.java
+++ src/main/java/com/thealgorithms/strings/CharactersSame.java
@@ -1,17 +1,6 @@
 package com.thealgorithms.strings;
 
 public class CharactersSame {
-
-    /**
-     * Driver Code
-     */
-    public static void main(String[] args) {
-        assert isAllCharactersSame("");
-        assert !isAllCharactersSame("aab");
-        assert isAllCharactersSame("aaa");
-        assert isAllCharactersSame("11111");
-    }
-
     /**
      * check if all the characters of a string are same
      *
@@ -19,7 +8,14 @@ public class CharactersSame {
      * @return {@code true} if all characters of a string are same, otherwise
      * {@code false}
      */
+    
+    //@ requires s.length() < Integer.MAX_VALUE - 2;
+    //@ ensures ((\forall int x; 0 <= x < s.length(); s.charAt(x) == s.charAt(0) ) <==> \result);
     public static boolean isAllCharactersSame(String s) {
+
+        //@ maintaining (1 <= i <= s.length()) || (i==1 && s.length() == 0);
+        //@ maintaining (\forall int k; 0 <= k < i; s.charAt(k) == s.charAt(0));
+        //@ decreases length - i;
         for (int i = 1, length = s.length(); i < length; ++i) {
             if (s.charAt(i) != s.charAt(0)) {
                 return false;
