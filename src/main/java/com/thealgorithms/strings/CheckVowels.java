1a84252ae90da5faf412f279ee68cef989783571
leodavidbr <rbdivadoel@gmail.com>
2023-12-10 16:50:21 -0300

    checkVowels incomplete

--- src/main/java/com/thealgorithms/strings/CheckVowels.java
+++ src/main/java/com/thealgorithms/strings/CheckVowels.java
@@ -12,7 +12,8 @@ import java.util.Set;
 public class CheckVowels {
 
     //@ public invariant VOWELS != null;
-    //@ public invariant (\forall char c; VOWELS.contains(c) <==> (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'));
+    // @ public invariant (\forall char c; VOWELS.contains(c) <==> (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'));
+    //@ spec_public
     private static final Set<Character> VOWELS = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
 
     /**
@@ -21,11 +22,22 @@ public class CheckVowels {
      * @param input a string
      * @return {@code true} if given string has vowels, otherwise {@code false}
      */
+    //@ requires input.length() < Integer.MAX_VALUE - 1;
+    //@ requires input != null;
+    //@ ensures (!(\forall int x; 0 <= x < input.length(); !VOWELS.contains(Character.toLowerCase(input.charAt(x))) ) <==> \result);
+    //@ also
+    //@ requires input == null;
+    //@ ensures \result == false;
     public static boolean hasVowels(String input) {
         if (input == null) {
             return false;
         }
         input = input.toLowerCase();
+
+        //@ maintaining 0 <= i <= input.length();
+        //@ loop_writes \nothing;
+        //@ maintaining (\forall int k; 0 <= k < i; !(VOWELS.contains(input.charAt(k))) );
+        //@ decreases input.length() - i;
         for (int i = 0; i < input.length(); i++) {
             if (VOWELS.contains(input.charAt(i))) {
                 return true;
