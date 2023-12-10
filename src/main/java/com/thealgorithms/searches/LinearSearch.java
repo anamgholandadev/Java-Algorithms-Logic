e367f2e90da8d9f9ac70416be44c2ee6414e6197
leodavidbr <rbdivadoel@gmail.com>
2023-12-10 16:17:32 -0300

    LinearSearch working without interface implementation

--- src/main/java/com/thealgorithms/searches/LinearSearch.java
+++ src/main/java/com/thealgorithms/searches/LinearSearch.java
@@ -1,6 +1,6 @@
 package com.thealgorithms.searches;
 
-import com.thealgorithms.devutils.searches.SearchAlgorithm;
+// import com.thealgorithms.devutils.searches.SearchAlgorithm;
 import java.util.Random;
 import java.util.stream.Stream;
 
@@ -27,16 +27,31 @@ public class LinearSearch  {
      * @param value Key being searched for
      * @return Location of the key
      */
+    // @ requires array.length < Integer.MAX_VALUE;
+    // @ requires (\forall int x; 0 <= x < array.length; \typeof(value) <: \typeof(array[x]));
+    // @ requires !(\forall int x; 0 <= x < array.length; array[x].compareTo(value) != 0);
+    // @ ensures \result >= 0 && array[\result].compareTo(value) == 0;
+    // @ also
+    // @ requires array.length < Integer.MAX_VALUE;
+    // @ requires (\forall int x; 0 <= x < array.length; \typeof(value) <: \typeof(array[x]));
+    // @ requires (\forall int x; 0 <=x < array.length; array[x].compareTo(value) != 0);
+    // @ ensures \result == -1;
+    
     //@ requires array.length < Integer.MAX_VALUE;
-    //@ requires !(\forall int x; 0 <=x < array.length; array[x].compareTo(value) != 0);
-    //@ ensures array[\result].compareTo(value) == 0;
-    //@ also
-    //@ requires array.length < Integer.MAX_VALUE;
-    //@ requires (\forall int x; 0 <=x < array.length; array[x].compareTo(value) != 0);
-    //@ ensures \result == -1;
-    public <T extends Comparable<T>> int find(T[] array, T value) {
+    //@ requires (\forall int x; 0 <= x < array.length; \typeof(value) <: \typeof(array[x]));
+    //@ ensures (!(\forall int x; 0 <= x < array.length; array[x].compareTo(value) != 0)) ==> array[\result].compareTo(value) == 0;
+    //@ ensures (\forall int x; 0 <=x < array.length; array[x].compareTo(value) != 0) ==> \result == -1;
+    //@ pure
+    public <T extends Comparable<T>> int find(/*@ non_null @*/ T /*@ non_null @*/[] array, /*@ non_null @*/ T value) {
+
+        //@ loop_writes \nothing;
+        //@ maintaining 0 <= i <= array.length;
+        //@ maintaining (\forall int k; 0 <= k < i; array[k].compareTo(value) != 0);
+        //@ decreases array.length - i;
         for (int i = 0; i < array.length; i++) {
+            //@ assert \typeof(value) <: \typeof(array[i]);
             if (array[i].compareTo(value) == 0) {
+                //@ assert array[i].compareTo(value) == 0;
                 return i;
             }
         }
